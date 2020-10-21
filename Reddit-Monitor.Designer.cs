de1f914352f18f22bb05a26dbc7e8db367e0d83c
Ben Ryan <benryan03@gmail.com>
2020-10-21 19:06:12 -0400

    Cleanup

---
+++ Reddit-Monitor.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Reddit_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.postTitleLabel = new System.Windows.Forms.Label();
+            this.postSubredditLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1002, 712);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.refreshTimer.Interval = 1000;
+            this.refreshTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // postTitleLabel
+            // 
+            this.postTitleLabel.AutoSize = true;
+            this.postTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postTitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.postTitleLabel.Name = "postTitleLabel";
+            this.postTitleLabel.Size = new System.Drawing.Size(322, 55);
+            this.postTitleLabel.TabIndex = 2;
+            this.postTitleLabel.Text = "postTitleLabel";
+            // 
+            // postSubredditLabel
+            // 
+            this.postSubredditLabel.AutoSize = true;
+            this.postSubredditLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postSubredditLabel.Location = new System.Drawing.Point(12, 64);
+            this.postSubredditLabel.Name = "postSubredditLabel";
+            this.postSubredditLabel.Size = new System.Drawing.Size(439, 55);
+            this.postSubredditLabel.TabIndex = 3;
+            this.postSubredditLabel.Text = "postSubredditLabel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 712);
+            this.Controls.Add(this.postSubredditLabel);
+            this.Controls.Add(this.postTitleLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Reddit Monitor";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer refreshTimer;
+        private System.Windows.Forms.Label postTitleLabel;
+        private System.Windows.Forms.Label postSubredditLabel;
+    }
+}
+
---
+++ Reddit-Monitor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace Reddit_Monitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            postTitleLabel.Text = "";
+            postSubredditLabel.Text = "";
+            refreshTimer.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Load newest images page from Reddit
+            string url = "https://www.reddit.com/domain/i.redd.it/new/";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            // Extract newest image post
+            var newestPost = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/div[1]");
+
+            // Extract details from post
+            string imageUrl = newestPost.GetAttributeValue("data-url", "").ToString();
+            string subreddit = newestPost.GetAttributeValue("data-subreddit", "").ToString();
+            string title = doc.DocumentNode.SelectSingleNode("/html/body/div[4]/div/div[1]/div[2]/div[1]/p[1]/a").InnerText;
+
+            // Download image
+            WebClient wc = new WebClient();
+            byte[] bytes = wc.DownloadData(imageUrl);
+            MemoryStream ms = new MemoryStream(bytes);
+            Image img = Image.FromStream(ms);
+
+            // Update display
+            pictureBox1.Image = img;
+            postTitleLabel.Text = title;
+            postSubredditLabel.Text = "/r/" + subreddit;
+
+            // Write to log file
+            using (StreamWriter log = new StreamWriter(@"log.txt", true))
+            {
+                log.Write(DateTime.Now + " | ");
+                log.Write(imageUrl + " | ");
+                log.Write("/r/" + subreddit + " | ");
+                log.Write(title + "\n");
+            }
+        }
+    }
+}
